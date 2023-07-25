0ce0691aff1f1365a83656a9d2ae41e6721de42a
Adir Noyman <ofahol2007@hotmail.com>
2023-07-25 21:23:42 +0300

    connected my app to RDS

--- appy/appy/settings.py
+++ appy/appy/settings.py
@@ -82,10 +82,29 @@ WSGI_APPLICATION = 'appy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# My Postgress database on AWS RDS
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'django_database_1',
+
+        'USER': 'adir',
+
+        'PASSWORD': os.environ.get('AWS_RDS_POSTGRESS_PASSWORD'),
+
+        'HOST': os.environ.get('AWS_RDS_POSTGRESS_ENDPOINT'),
+
+        'PORT': '5432',
     }
 }
 
